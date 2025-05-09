319549d4b96652cf4b7804b40dd2fd99b5ba0e30
sp6822 <sp6822@bs.amalne.k12.il>
2025-05-09 16:23:14 +0300

    task11

--- app/src/main/java/com/example/task11/MainActivity.java
+++ app/src/main/java/com/example/task11/MainActivity.java
@@ -6,50 +6,50 @@ import android.widget.Button;
 import android.widget.RadioGroup;
 import android.widget.Switch;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    View rootView;
+
     Switch sw;
-     RadioGroup RG;
-     Button change;
-     ConstraintLayout screen;
+    RadioGroup RG;
+    Button change;
+    ConstraintLayout screen;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        screen = (ConstraintLayout) findViewById(R.id.main);
+
         sw = findViewById(R.id.sw);
         RG = findViewById(R.id.RG);
         change = findViewById(R.id.change);
+        screen = findViewById(R.id.main);
     }
-    public void setColor(View view)
-    {
-        if (sw.isChecked())
-        {
-            change_color();
+
+    public void setColor(View view) {
+        if (sw.isChecked()) {
+            updateBackground();
         }
     }
-        public void check(View view) {
-            if (!sw.isChecked()) {
-                change_color();
-            }
+
+    public void check(View view) {
+        if (!sw.isChecked()) {
+            updateBackground();
         }
-        public void change_color() {
-            int selectedId = RG.getCheckedRadioButtonId();
-            if (selectedId == R.id.rb1) {
-                screen.setBackgroundResource(R.drawable.red);
-            } else if (selectedId == R.id.rb2) {
-                screen.setBackgroundResource(R.drawable.blue1);
-            } else if (selectedId == R.id.rb3) {
-                screen.setBackgroundResource(R.drawable.pink);
-            } else if (selectedId == R.id.rb4) {
-                screen.setBackgroundResource(R.drawable.purple);
-            }
+    }
+
+    private void updateBackground() {
+        int selected = RG.getCheckedRadioButtonId();
+
+        if (selected == R.id.rb1) {
+            screen.setBackgroundResource(R.drawable.red);
+        } else if (selected == R.id.rb2) {
+            screen.setBackgroundResource(R.drawable.blue1);
+        } else if (selected == R.id.rb3) {
+            screen.setBackgroundResource(R.drawable.pink);
+        } else if (selected == R.id.rb4) {
+            screen.setBackgroundResource(R.drawable.purple);
         }
+    }
 }
